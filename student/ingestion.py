65c2e73f855af2d0f6bbf6f1a84c50cd738f37f2
Vinicius Zani <vinicius.zani04@gmail.com>
2026-08-14 09:59:29 -0300

    fix bugs index

--- student/ingestion.py
+++ student/ingestion.py
@@ -139,7 +139,7 @@ def ingest_repo(repo_path: str, max_chunk_size: int) -> list[dict]:
         for doc in chunk_text(content, max_chunk_size):
             all_chunks.append({
                 "text": doc.page_content,
-                "file_path": py_file.as_posix(),
+                "file_path": md_file.as_posix(),
                 "first_character_index": doc.metadata["start_index"],
                 "last_character_index": (doc.metadata["start_index"]
                                          + len(doc.page_content)),
